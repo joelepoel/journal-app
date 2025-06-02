92eb2fb61252cdef16838958867956a660946e49
joelepoel <louispjoel@gmail.com>
2025-06-02 03:19:16 +0200

    added admin functionality for accounts app

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ['id', 'username', 'email', 'is_staff', 'is_active']
+    list_filter = ['is_staff', 'is_active']
+    search_fields = ['username__istartswith', 'email__istartswith']
+    ordering = ['id']

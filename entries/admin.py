a68087077077bba9d79f312cf314962dd0a1d34c
joelepoel <louispjoel@gmail.com>
2025-06-02 03:06:53 +0200

    updated admin.py

--- entries/admin.py
+++ entries/admin.py
@@ -5,16 +5,16 @@ from .models import Profile, Entry
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ['id']
     list_per_page = 10
-    search_fields = ['id__startswith']
+    search_fields = ['id']
     list_filter = ['id']
 
 @admin.register(Entry)
 class EntryAdmin(admin.ModelAdmin):
     list_display = ['id', 'title', 'get_profile']
     list_per_page = 10
-    search_fields = ['id__startswith']
-    list_filter = ['id']
-    list_select_related = ['profile']
+    search_fields = ['title__istartswith']
+    list_filter = ['user']
+    list_select_related = ['user']
 
     def get_profile(self, Entry):
         return Entry.profile.id
